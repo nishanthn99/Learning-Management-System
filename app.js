3d5220f25b2089745a9e86ddf2844aaf1b916a85
nishanthn99 <21e32.nishanth@sjec.ac.in>
2024-09-12 19:32:37 +0530

    created signup,login,forotpass

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
-const express=require('express')
-const app=express()
+const express=require('express');
+const app=express();
+const bodyParser=require('body-parser');
+
+app.set('view engine','ejs')
+
 
 app.get('/',(req,res)=>{
-    res.send('Learner System Management')
+    res.render('index',{title:"Welcome to Learning Management System"})
+});
+
+app.get('/signup',(req,res)=>{
+    res.render('signup',{title:"Create a New Account in Learning Management System"})
+});
+
+app.post('/newuser',(req,res)=>{
+    const {username,password}=req.body;
 })
+
+app.get('/login',(req,res)=>{
+    res.render('login',{title:"LogIn To Your Learning Management System Account"})
+});
+
+app.post('/dashboard',(req,res)=>{
+    const {username,password}=req.body;
+})
+
+app.get('/forgotpassword',(req,res)=>{
+    res.render('forgotpassword',{title:"Forgot Password in Learning Management System"})
+});
+
+app.post('/resetpassword',(req,res)=>{
+    const {email}=req.body;
+})
+
+
+module.exports=app;
