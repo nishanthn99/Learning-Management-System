31eb5f732685d00115addac7590ad456dec7792d
nishanthn99 <21e32.nishanth@sjec.ac.in>
2024-09-13 17:20:33 +0530

    dbs added

---
+++ models/chapter.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Chapter extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Chapter.init({
+    chaptertitle: DataTypes.STRING,
+    description: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Chapter',
+  });
+  return Chapter;
+};
